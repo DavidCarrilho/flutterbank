d552eb0ae35f67745e1b102fe9467cbfc25801c7
DavidCarrilho <luan.david.oliver@gmail.com>
2022-03-04 01:27:19 -0300

    feat: create components

--- lib/app/pages/home_page.dart
+++ lib/app/pages/home_page.dart
@@ -10,28 +10,54 @@ class HomePage extends StatelessWidget {
         title: Text('Trnsferências'),
         centerTitle: true,
       ),
-      body: Column(
-        children: <Widget>[
-          Card(
-            child: ListTile(
-              leading: Icon(Icons.monetization_on),
-              title: Text('100.0'),
-              subtitle: Text('1000'),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              leading: Icon(Icons.monetization_on),
-              title: Text('500.0'),
-              subtitle: Text('2000'),
-            ),
-          ),
-        ],
-      ),
+      body: ListaTransferencia(),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         child: Icon(Icons.add),
       ),
     );
   }
+}
+
+class ListaTransferencia extends StatelessWidget {
+  const ListaTransferencia({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        ItemTransferencia(Transferencia(100.00, 12345)),
+        ItemTransferencia(Transferencia(100.00, 12345)),
+        ItemTransferencia(Transferencia(100.00, 12345)),
+      ],
+    );
+  }
+}
+
+class ItemTransferencia extends StatelessWidget {
+  final Transferencia _transferencia;
+
+  ItemTransferencia(this._transferencia);
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(_transferencia.valor.toString()),
+        subtitle: Text(_transferencia.numeroConta.toString()),
+      ),
+    );
+  }
+}
+
+class Transferencia {
+  final double valor;
+  final int numeroConta;
+
+  Transferencia(
+    this.valor,
+    this.numeroConta,
+  );
 }
