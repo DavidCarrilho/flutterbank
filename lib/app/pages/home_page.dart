e72025d7f7a841d9f704d73c533a34f029849908
DavidCarrilho <luan.david.oliver@gmail.com>
2022-03-05 00:31:05 -0300

    feat: create field news

--- lib/app/pages/home_page.dart
+++ lib/app/pages/home_page.dart
@@ -17,11 +17,39 @@ class FormularioTranferencia extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("data"),),
-      body: Text('Test'),
+      appBar: AppBar(
+        title: Text("data"),
+      ),
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              style: TextStyle(fontSize: 24.0),
+              decoration: InputDecoration(
+                  labelText: 'Número da conta',
+                  hintText: '0000',
+                  icon: Icon(Icons.monetization_on)),
+              keyboardType: TextInputType.number,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              style: TextStyle(fontSize: 24.0),
+              decoration: InputDecoration(
+                  labelText: 'Valor',
+                  hintText: '0',
+                  icon: Icon(Icons.monetization_on)),
+              keyboardType: TextInputType.number,
+            ),
+          ),
+          RaisedButton(
+            onPressed: () {},
+            child: Text('Confirmar'),
+          )
+        ],
+      ),
     );
   }
 }
-
-
-
