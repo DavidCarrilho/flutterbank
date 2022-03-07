3df371f4f2c1645c35761ff727b05d8eb87a58ea
DavidCarrilho <luan.david.oliver@gmail.com>
2022-03-06 23:26:40 -0300

    feat: create theme

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:simplebank/app/pages/lista_tranferencia.dart';
 import 'package:simplebank/app/pages/pages.dart';
 
 class AppWidget extends StatelessWidget {
@@ -7,8 +6,15 @@ class AppWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'SimpleBank',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: ListaTransferencia(),
+      theme: ThemeData(
+        primaryColor: Colors.green[900],
+        accentColor: Colors.blueAccent[700],
+        buttonTheme: ButtonThemeData(
+          buttonColor: Colors.blueAccent[700],
+          textTheme: ButtonTextTheme.primary,
+        ),
+      ),
+      home: HomePage(),
     );
   }
 }
--- lib/app/pages/home_page.dart
+++ lib/app/pages/home_page.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:simplebank/app/components/components.dart';
+import 'package:simplebank/app/pages/lista_tranferencia.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: FormularioTranferencia(),
-    );
+    return ListaTransferencia();
   }
 }
