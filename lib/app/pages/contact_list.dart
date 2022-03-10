dccba8fc06d78f5213032d5aac13e4c5827dba10
DavidCarrilho <luan.david.oliver@gmail.com>
2022-03-09 23:22:48 -0300

    feat: testing the ConnectionState

--- lib/app/pages/contact_list.dart
+++ lib/app/pages/contact_list.dart
@@ -1,6 +1,7 @@
 import 'dart:developer' as developer;
 
 import 'package:flutter/material.dart';
+import 'package:flutterbank/app/design_system/colors/colors.dart';
 import 'package:flutterbank/app/models/models.dart';
 import 'package:flutterbank/db/app_db.dart';
 
@@ -21,19 +22,41 @@ class ContactList extends StatelessWidget {
       ),
       body: FutureBuilder<List<Contact>>(
         initialData: [],
-        future: Future.delayed(Duration(seconds: 1)).then((value) => findAll()),
+        future: findAll(),
         builder: (context, snapshot) {
-          final List<Contact> contacts = snapshot.data;
-          return ListView.builder(
-            physics: const BouncingScrollPhysics(),
-            itemBuilder: (context, index) {
-              final Contact contact = contacts[index];
-              return ContactWidget(
-                contact: contact,
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+              break;
+            case ConnectionState.waiting:
+              return Center(
+                child: CircularProgressIndicator(),
               );
-            },
-            itemCount: contacts.length,
-          );
+              break;
+            case ConnectionState.active:
+              break;
+            case ConnectionState.done:
+              final List<Contact> contacts = snapshot.data;
+              return ListView.builder(
+                physics: const BouncingScrollPhysics(),
+                itemBuilder: (context, index) {
+                  final Contact contact = contacts[index];
+                  return ContactWidget(
+                    contact: contact,
+                  );
+                },
+                itemCount: contacts.length,
+              );
+              break;
+          }
+          return Center(
+              child: Text(
+            'Erro no carregamento!',
+            style: TextStyle(
+              fontSize: 24.0,
+              color: kColorDeepPurple,
+              fontWeight: FontWeight.bold,
+            ),
+          ));
         },
       ),
       floatingActionButton: FloatingActionButton(
