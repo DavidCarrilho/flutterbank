47d7cf6a53179a1ab67bca1fd99a72d3f0077feb
DavidCarrilho <luan.david.oliver@gmail.com>
2022-03-12 01:28:16 -0300

    feat: using listview with horizontal scroll

--- lib/app/pages/dashboard_page.dart
+++ lib/app/pages/dashboard_page.dart
@@ -18,36 +18,40 @@ class DashboardPage extends StatelessWidget {
         title: Text(_appTitle),
         centerTitle: true,
       ),
-      body: Container(
-        margin: EdgeInsets.all(8.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Container(
-              padding: EdgeInsets.symmetric(vertical: 30.0),
-              width: double.infinity,
-              color: kColorNeutralPurple,
-              child: Column(
-                children: <Widget>[
-                  FlutterLogo(
-                    size: 180.0,
-                    colors: Colors.deepPurple,
-                  ),
-                  const SizedBox(height: 20.0),
-                  Text(
-                    'FlutterBank',
-                    style: TextStyle(
-                      color: kColorDeepPurple,
-                      fontSize: 32.0,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Row(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.all(8.0),
+            padding: EdgeInsets.symmetric(vertical: 30.0),
+            width: double.infinity,
+            color: kColorNeutralPurple,
+            child: Column(
               children: <Widget>[
+                FlutterLogo(
+                  size: 180.0,
+                  colors: Colors.deepPurple,
+                ),
+                const SizedBox(height: 20.0),
+                Text(
+                  'FlutterBank',
+                  style: TextStyle(
+                    color: kColorDeepPurple,
+                    fontSize: 32.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            height: 100.0,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              physics: const BouncingScrollPhysics(),
+              children: <Widget>[
+                const SizedBox(width: 8.0),
                 _FeatureItemWidget(
                   name: 'Transferência',
                   icon: Icons.monetization_on,
@@ -55,20 +59,16 @@ class DashboardPage extends StatelessWidget {
                 ),
                 const SizedBox(width: 8.0),
                 _FeatureItemWidget(
-                  name: 'Histórico',
-                  icon: Icons.description,
-                  onClick: () => developer.log('Historico')
-                ),
+                    name: 'Histórico',
+                    icon: Icons.description,
+                    onClick: () => developer.log(
+                          'Historico',
+                        )),
                 const SizedBox(width: 8.0),
-                _FeatureItemWidget(
-                  name: 'Histórico',
-                  icon: Icons.description,
-                  onClick: () => developer.log('Historico')
-                ),
               ],
-            )
-          ],
-        ),
+            ),
+          )
+        ],
       ),
     );
   }
@@ -95,24 +95,26 @@ class _FeatureItemWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      color: Theme.of(context).primaryColor,
-      child: InkWell(
-        onTap: onClick,
-        child: Container(
-          height: 100.0,
-          width: 120.0,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Icon(icon, size: 30.0, color: kColorNeutralPurple),
-              const SizedBox(height: 12.0),
-              Text(
-                name,
-                style: TextStyle(color: kColorNeutralPurple),
-              ),
-            ],
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 8.0),
+      child: Material(
+        color: Theme.of(context).primaryColor,
+        child: InkWell(
+          onTap: onClick,
+          child: Container(
+            width: 120.0,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Icon(icon, size: 30.0, color: kColorNeutralPurple),
+                const SizedBox(height: 12.0),
+                Text(
+                  name,
+                  style: TextStyle(color: kColorNeutralPurple),
+                ),
+              ],
+            ),
           ),
         ),
       ),
