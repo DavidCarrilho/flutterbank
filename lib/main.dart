e1d29c9765205b7cf25af1c6f88d1ca5ca4bd3cb
DavidCarrilho <luan.david.oliver@gmail.com>
2022-03-20 01:01:46 -0300

    feat: add http package and dreate webclien with findall transaction method

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutterbank/http/web_client.dart';
 
 import 'app/app_widget.dart';
 
-void main() => runApp(AppWidget());
+void main() {
+  runApp(AppWidget());
+  findAll();
+}
