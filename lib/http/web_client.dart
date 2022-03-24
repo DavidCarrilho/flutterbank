25d6fc936f919a8e361659718044ec1030ce73e5
DavidCarrilho <luan.david.oliver@gmail.com>
2022-03-24 02:57:45 -0300

    feat: create save method

--- lib/http/web_client.dart
+++ lib/http/web_client.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 
 import 'package:flutterbank/app/models/models.dart';
-import 'package:flutterbank/app/pages/pages.dart';
 import 'package:http/http.dart';
 import 'package:http_interceptor/http_interceptor.dart';
 import 'dart:developer' as developer;
@@ -26,12 +25,14 @@ class LoggingInterceptor implements InterceptorContract {
   }
 }
 
+final Client client =
+    HttpClientWithInterceptor.build(interceptors: [LoggingInterceptor()]);
+
+const String baseUrl = 'http://192.168.1.109:8080/transactions';
+
 Future<List<Transaction>> findAll() async {
-  final Client client =
-      HttpClientWithInterceptor.build(interceptors: [LoggingInterceptor()]);
-  final Response response = await client
-      .get('http://192.168.1.109:8080/transactions')
-      .timeout(Duration(seconds: 5));
+  final Response response =
+      await client.get(baseUrl).timeout(Duration(seconds: 5));
   final List<dynamic> decodeedJson = jsonDecode(response.body);
   final List<Transaction> transactions = [];
   for (Map<String, dynamic> transactionJson in decodeedJson) {
@@ -48,6 +49,34 @@ Future<List<Transaction>> findAll() async {
   return transactions;
 }
 
-// void save(Transaction transaction) {
-//   post(url);
-// }
+Future<Transaction> save(Transaction transaction) async {
+  final Map<String, dynamic> transactionMap = {
+    'value': transaction.value,
+    'contact': {
+      'name': transaction.contact.name,
+      'accountNumber': transaction.contact.accountNumber,
+    },
+  };
+  final String transactionJson = jsonEncode(transactionMap);
+
+  final Response response = await client.post(
+    baseUrl,
+    headers: {
+      'Content-type': 'application/json',
+      'password': '1000',
+    },
+    body: transactionJson,
+  );
+
+  Map<String, dynamic> json = jsonDecode(response.body);
+
+  final Map<String, dynamic> contatcJson = json['contact'];
+  return Transaction(
+    value: json['value'],
+    contact: Contact(
+      id: 0,
+      name: json['name'],
+      accountNumber: json['accountNumber'],
+    ),
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutterbank/app/models/models.dart';
 import 'package:flutterbank/http/web_client.dart';
 
 import 'app/app_widget.dart';
@@ -6,5 +7,9 @@ import 'dart:developer' as developer;
 
 void main() {
   runApp(AppWidget());
+  save(Transaction(
+          value: 200,
+          contact: Contact(id: 0, name: 'Jorge', accountNumber: 2000)))
+      .then((transaction) => print(transaction));
   findAll().then((transaction) => developer.log('Nova transação $transaction'));
 }
